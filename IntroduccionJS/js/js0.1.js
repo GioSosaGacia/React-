107f0b39f4912c48fe5ee0b63b6c44fb34d93a22
GioSosaGacia <giovanni-sosa-12@outlook.com>
2025-06-09 20:07:54 -0600

    Objetos

--- IntroduccionJS/js/js0.1.js
+++ IntroduccionJS/js/js0.1.js
@@ -21,7 +21,8 @@ Objetos: Son una coleccion de propiedades, y una propiedad es una asociacion ent
 const producto = {
     nombre: "Table",
     precio: 1500,
-    disponible: false
+    disponible: false,
+    imagen: " "
 }
 
 console.log(producto);
@@ -31,11 +32,11 @@ console.table(producto);
 // Podemos acceder a las propiedades de un objeto 
 // console.log(producto.nombre)
 // destructuring se usa para extraer valores de un objeto 
-const { nombre, precio, disponible } = producto;
+const { nombre, precio, disponible,imagen } = producto;
 console.log(nombre);
 console.log(precio);
 console.log(disponible);
-s
+
 
 // Object Literal Enhancement sirve para colocar variables dentro de un objeto
 const autenticado = true;
@@ -49,13 +50,47 @@ const newObject = {
 
 console.log(newObject);
 
-// Con object.freeze bloqueamos la posibilidad de mdificar un objeto
+// Con object.freeze bloqueamos la posibilidad de modificar un objeto
 Object.freeze(producto);
 // No permite modificar ni eliminar las propiedades de un objeto
 Object.seal(producto); 
 
 // Manipulacion de objetos, podemos modificar, agregar o eliminar un objeto de la siguiente manera;
 producto.nombre = "Audifonos";
-producto.imagen = "imagen.png";
-delete producto.precio;
-console.log(producto);
+producto.imagen = "imagen";
+// delete producto.precio;
+console.log("Hola" + " " + producto.nombre, producto.imagen);
+
+// Destructuring de objetos de 2 o mas objetos 
+const ford = {
+    marca: "Mustang",
+    motor: "V8",
+    color: "Rojo"
+}
+
+const honda = {
+    marca: "Civic",
+    motor: "4 cilindros",
+    transmisones: {
+        standar: "6 velicidades",
+        standar: "5 velocidades",
+        automatica: "5 velocidades",
+        cvt: "5 velocidades"
+    }
+}
+
+const { marca } = ford
+//Con los dos: podemos renombrar la variable para que no marque error y cuando tenemos un objeto dentro de otro objeto usamos las {} para acceder a su valor.
+const { marca: marcaHonda, transmisones: {automatica}} = honda
+
+console.log(marca);
+console.log(marcaHonda);
+console.log(automatica);
+
+// Unir dos o mas objetos:usando el spread operator
+const carrito = {
+    cantidad: 1,
+    ...ford
+}
+
+console.log(carrito);
