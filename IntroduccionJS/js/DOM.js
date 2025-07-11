bb3d3771979e2e3b8a3b2d6318734b5db76e8751
GioSosaGacia <giovanni-sosa-12@outlook.com>
2025-07-10 22:23:56 -0600

    add events using addEventListener

--- IntroduccionJS/js/DOM.js
+++ IntroduccionJS/js/DOM.js
@@ -1,7 +1,7 @@
 // DOM, la mayoria de iteraciones requieren o inician con un "selectores" -> siempre inician con document 
 // document hace referencia a tu documento HTML, EN MODE DESARROLLO en consola writte document 
 
-// querySelector(retorna hasta un elemento) y querySelectorAll(selecciona todos lo elementos) -->
+// querySelector(retorna hasta un elemento, el primero qu coincida con la condición) y querySelectorAll(selecciona todos lo elementos) -->
 // getElemetById --> permitre seleccionar un elemento html por su id
 
 // .clase,  #id, :last-of-type
@@ -35,3 +35,55 @@ console.log(enlace);
 heading.textContent = 'Un nuevo heading '
 heading.id = 'Un nuevo Id'
 
+//Para eliminar usamos remove, eliminamos el id, tal elemento o atributo va entre ' ' 
+// Si solo uso remove eliminara el elemento html 
+heading.removeAttribute('id');
+
+const inputNombre = document.querySelector('#nombre');
+inputNombre.value = 'Un nuevo valor... desde el DOM'
+// enlaces.forEach( enlace => enlace.textContent = 'Nuevo enlace');
+
+
+
+
+
+
+
+// Eventos del DOM -> https://developer.mozilla.org/es/docs/Web/API/Element
+// Eventos permiten hacer interactiva una pagina web: Onclick()
+heading.textContent = 'Un nuevo titulo';
+
+// Usamo el metodo addEventListener para agregar un evento, click, seguido de una funcion flecha o llamada callBack, al presionar el texto dentro del  heading cambia el texto por el evento click
+heading.addEventListener('click', () => {
+    heading.textContent = 'Nuevo heading al dar click...'
+})
+
+heading.addEventListener( 'mouseenter', () => {
+    heading.textContent = 'nuevo headin al dar click'
+})
+
+heading.addEventListener('mouseleave', () => {
+    heading.textContent = 'Saliendo';
+})
+
+
+// e = event o evento, arroja informacion sobre el evento que se esta llevando a cabo 
+// target --> nos muestra el detalle, sobre que elemento se ejecuto el disparador o a que le dimos click 
+// ahi elementos que por defecto tienen un comportamiento como en los enlaces dar un salto de linea
+// Para prevenir u omitir tal accion por defaul podemos usar ---->  preventDefault()
+enlaces.forEach( enlace => {
+    enlace.addEventListener('click', (e) => {
+        e.preventDefault();
+        e.target.textContent = 'Diste click'
+        console.log(e.target);
+        // console.log('Diste un clieck en un enlace');
+    })
+})
+
+
+
+
+
+
+
+// Inputs 
